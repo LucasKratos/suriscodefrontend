68a372d0a27646bbe65f1aa79461be59773e21cf
Spagnoli Berzobohaty, Lucas Agustin <laspagnoli-ext@aa2000.com.ar>
2024-12-14 22:40:39 -0300

    Cambios en .test file, se elimina por reemplazo de componentes

--- src/MarketplaceDasboard.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import MarketplaceDashboard from './components/MarketplaceDashboard';
-
-test('renders learn react link', () => {
-  render(<MarketplaceDashboard />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
